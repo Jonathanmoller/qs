4a3b644930ee41b9a76910175a0770f3a3c8647b
Philip Englund <phille96@gmail.com>
2019-09-18 11:11:49 +0200

    initial commit

---
+++ Sources/AssemblyVersionAuto.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+
+
+
+[assembly: AssemblyCopyright("Copyright ? 2019 Episerver")]
+[assembly: AssemblyVersion("13.7.1.0")]
+[assembly: AssemblyInformationalVersion("13.7.1")]
+[assembly: AssemblyFileVersion("13.7.1.3760")]
---
+++ Sources/EPiServer.Reference.Commerce.Site/Features/Product/Services/CatalogService.cs
@@ -0,0 +1,13 @@
+using EPiServer.ServiceApi.Commerce;
+using EPiServer.ServiceApi.Commerce.Models.Catalog;
+
+namespace EPiServer.Reference.Commerce.Site.Features.Product.Services
+{
+    public class CatalogService
+    {
+        public void CreateCatalog(Catalog catalog)
+        {
+            var serviceApi = EPiServer.ServiceApi
+        }
+    }
+}
